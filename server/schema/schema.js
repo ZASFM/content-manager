5ab5cb7671d329fbe5be541d52a437d627e0174c
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2023-02-02 20:53:22 +0100

    Setting global schemas

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 const {projects, clients}=require('../mockData');
-const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema}=require('graphql');
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList}=require('graphql');
 
 //Client type:
 const ClientType=new GraphQLObjectType({
@@ -20,10 +20,44 @@ const ClientType=new GraphQLObjectType({
    })
 })
 
+//Project type:
+const ProjectType=new GraphQLObjectType({
+   name:'Project',
+   fields:()=>({
+      id:{
+         type: GraphQLID,
+      },
+      name:{
+         type:GraphQLString,
+      },
+      description:{
+         type:GraphQLString,
+      },
+      status:{
+         type:GraphQLString,
+      },
+      //Getting the client correspondent project
+      client:{
+         type:ClientType,
+         resolve(parent,args){
+            return clients.find(client=>client.id===parent.clientId)
+         }
+      }
+   })
+})
+
 const RootQuery=new GraphQLObjectType({
    name:'RootQueryType',
    fields:{
+      //All clients:
       clients:{
+         type: new GraphQLList(ClientType),
+         resolve(parent,args){
+            return clients;
+         }
+      },
+      //Single client:
+      client:{
          type:ClientType,
          args:{
             //Getting a single user with id:
@@ -34,6 +68,25 @@ const RootQuery=new GraphQLObjectType({
          //Returning the clients with that id:
          resolve(parent,args){
             return clients.find(client=>client.id===args.id)
+         }
+      },
+      //All projects:
+      projects:{
+         type:new GraphQLList(ProjectType),
+         resolve(parent,args){
+            return projects
+         }
+      },
+      //Single project:
+      project:{
+         type:ProjectType,
+         args:{
+            id:{
+               type:GraphQLID,
+            }
+         },
+         resolve(parent,args){
+            return projects.find(project=>project.id===args.id)
          }
       }
    }
