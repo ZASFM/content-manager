f029fd9ee85d052e4d1685e68222362f2008a1d5
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2023-02-06 21:17:35 +0100

    apollo error handling, mutation query

--- client/src/components/ClientRow.js
+++ client/src/components/ClientRow.js
@@ -1,6 +1,26 @@
 import { FaTrashAlt } from 'react-icons/fa';
+import { useMutation } from '@apollo/client';
+import { DELETE_CLIENT } from '../mutations/clientMutations';
+import { GET_CLIENTS } from './queries/ClientQueries';
 
 const ClientRow = ({ client }) => {
+
+   const {deleteClient}=useMutation(DELETE_CLIENT,{
+      variables:{id:client.id},
+      //refetching the browser:
+      //refetchQueries:[{query:GET_CLIENTS}],
+      //refecthing the browser with the update the cache:
+      update(cache,{data:{deleteClient}}){
+         const {clients}=cache.readQuery({
+            query:GET_CLIENTS
+         });
+         cache.writeQuery({
+            query:GET_CLIENTS,
+            data:{clients:clients.filter(client=>client.id!==deleteClient.id)}
+         });;
+      }
+   });
+
    return (
       <tr>
          <td>{client.name}</td>
@@ -8,7 +28,9 @@ const ClientRow = ({ client }) => {
          <td>{client.phone}</td>
          <td>
             <button className="btn btn-danger btn-sm">
-               <FaTrashAlt />
+               <FaTrashAlt 
+                  onClick={deleteClient}
+               />
             </button>
          </td>
       </tr>
