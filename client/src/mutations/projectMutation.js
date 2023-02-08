af48d994ae2851f01e264c993870bb0423691857
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2023-02-08 13:44:35 +0100

    [error] not deleting project

--- client/src/mutations/projectMutation.js
+++ client/src/mutations/projectMutation.js
@@ -27,6 +27,15 @@ const ADD_PROJECT=gql`
    }
 `;
 
+const DELETE_PROJECT=gql`
+   mutation DeleteProject($id:ID!){
+      deleteProject(id:$id){
+         id
+      }
+   }
+`;
+
 export {
-   ADD_PROJECT
+   ADD_PROJECT,
+   DELETE_PROJECT
 }
